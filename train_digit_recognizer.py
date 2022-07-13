baa2c09ff722e2f6883d94421f50dc404c77f383
Amir Hossein <44730731+amirhosseinxd@users.noreply.github.com>
2022-07-13 18:38:24 +0430

    Add files via upload

--- train_digit_recognizer.py
+++ train_digit_recognizer.py
@@ -37,19 +37,20 @@ model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.3))
+model.add(Dropout(0.25))
 model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
 
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 print("The model has successfully trained")
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:0.13256817817687988')
-print('Test accuracy:0.9820013404768372')
+score = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
 
 model.save('mnist.h5')
 print("Saving the model as mnist.h5")
